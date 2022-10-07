429c185657e296cb2363303bf6d7a0d12109a971
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-06 22:32:19 -0600

    Capture rejected promise

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -34,7 +34,17 @@ const getDogPic = async () => {
     console.log(`Random dog image saved to file!`);
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: READY!!!';
 };
 
-getDogPic();
+console.log('1: Will get dog pics!');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  })
+  .catch((err) => {
+    console.log('ERROR');
+  });
