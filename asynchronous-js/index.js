f5b02797aab17e319598d1b336df7536ff5f7e74
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-06 22:05:30 -0600

    Flat structure of chain promises

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -2,21 +2,36 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log(
-          `Random dog image saved to file!, img: ${res.body.message}`
-        );
-      });
-    })
-    .catch((err) => {
-      console.log(`Err: ${err.message}`);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file!!');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write the file');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log(`Random dog image saved to file!`);
+  })
+  .catch((err) => {
+    console.log(`Err: ${err}`);
+  });
